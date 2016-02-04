75506ec2e398e19142616c322f5e1a94e3fd005b
Yui Moogle <mogle0927@gmail.com>
2016-02-04 18:15:11 +0900

    SDloop

--- PCA_2/info.h
+++ PCA_2/info.h
@@ -18,10 +18,12 @@ struct info
 	std::string dir_def;
 	std::string dir_out;
 	std::string dir_result;
+	std::string dir_s;
+	std::string dir_d;
 
 	int n; //症例数
 	int p; //軸数
-	int d; //軸数
+
 	int n_num;
 	//int num_p; //見たい主成分スコアの軸番号
 
@@ -33,9 +35,11 @@ struct info
 		dir_def = nari::file::add_delim(info.get_as_str("dir_def"));
 		dir_out = info.get_as_str("dir_out");
 		dir_result = info.get_as_str("dir_result");
+		dir_s = info.get_as_str("S"); //説明変数の数
+		dir_d = info.get_as_str("D"); //目的変数の数
+
 
 		p = info.get_as_int("P");
-		d = info.get_as_int("D");
 
 	}
 
--- PCA_2/main.cpp
+++ PCA_2/main.cpp
@@ -12,30 +12,50 @@ int main(int argn, const char *argc[]) {
 	info input_info;
 	input_info.input(argc[1]);
 
-	
-
-	saito::model<double> pca;
-	pca.load_with_N(input_info.dir_def,input_info.d); //•ÏŒ`ê‚Ì‚â‚Â
-	//pca.load(input_info.dir_def);
-	std::vector<double> result;
-	read_vector(result, input_info.dir_result);
-	Eigen::MatrixXd result_read = Eigen::Map<Eigen::MatrixXd>(&result[0], input_info.d, 1);
-	
-	Eigen::Matrix< double, Eigen::Dynamic, Eigen::Dynamic > Y;
-
-	pca.pre_image(Y,result_read);
-	
-	
-	std::string o_file = static_cast<std::string>(input_info.dir_out) + "/mat";
-	write_matrix_raw_and_txt(Y, o_file);
-	std::ofstream mat(o_file + ".csv");
-	for (int i = 0; i < Y.rows(); i++) {
-		for (int j = 0; j < Y.cols(); j++) {
-			mat << Y(i, j) << ",";
-		}
-		mat << std::endl;
+	std::vector<int> s;
+	std::vector<int> d;
+	std::ifstream s_path(input_info.dir_s);
+	std::ifstream d_path(input_info.dir_d);
+	std::string buf_s;
+	std::string buf_d;
+	while (s_path&& getline(s_path, buf_s))
+	{
+		s.push_back(std::stoi(buf_s));
+	}
+	while (d_path&& getline(d_path, buf_d))
+	{
+		d.push_back(std::stoi(buf_d));
 	}
 
+	for (int j = 0; j < s.size(); j++) {
+		for (int k = 0; k < d.size(); k++) {
+
+			saito::model<double> pca;
+			pca.load_with_N(input_info.dir_def, d[k]); //•ÏŒ`ê‚Ì‚â‚Â
+			//pca.load(input_info.dir_def);
+			std::vector<double> result;
+			std::stringstream dir_Result;
+			dir_Result << input_info.dir_result << "D" << d[k] << "S" << s[j] << "/" << "test.raw";
+			read_vector(result, dir_Result.str());
+			Eigen::MatrixXd result_read = Eigen::Map<Eigen::MatrixXd>(&result[0], d[k], 1);
+
+			Eigen::Matrix< double, Eigen::Dynamic, Eigen::Dynamic > Y;
+
+			pca.pre_image(Y, result_read);
+
+			std::stringstream O_file;
+			O_file << input_info.dir_out << "/D" << d[k] << "S" << s[j] << "/mat";
+			nari::system::make_directry(O_file.str());
+			write_matrix_raw_and_txt(Y, O_file.str());
+			std::ofstream mat(O_file.str() + ".csv");
+			for (int i = 0; i < Y.rows(); i++) {
+				for (int j = 0; j < Y.cols(); j++) {
+					mat << Y(i, j) << ",";
+				}
+				mat << std::endl;
+			}
+		}
+	}
 	return EXIT_SUCCESS;
 	system("pause");
 
